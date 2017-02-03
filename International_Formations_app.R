1d8541d7a6b3990105ca5720bf2945b1b25301c8
Erika Ito <etito@wisc.edu>
2017-02-03 12:38:30 -0600

    change formation grep

--- International_Formations_app.R
+++ International_Formations_app.R
@@ -88,7 +88,7 @@ CleanedDDWords<-gsub(","," ",DeepDiveData[,"words"])
 print(paste("Search for the word ' formation' in DeepDiveData sentences",Sys.time()))
 
 # Apply grep to the object cleaned words
-FormationHits<-parSapply(Cluster," formation",function(x,y) grep(x,y,ignore.case=TRUE, perl = TRUE),CleanedDDWords)
+FormationHits<-grep(" formation", ignore.case=TRUE, perl = TRUE, CleanedDDWords)
 # Extact DeepDiveData rows corresponding with formation hits
 SubsetDeepDive<-DeepDiveData[FormationHits,]
 
